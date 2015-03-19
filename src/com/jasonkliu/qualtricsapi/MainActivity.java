93a77aee285275030e979e6a635089429bf06aa1
Jason Liu <jason@jasonkliu.com>
2015-03-19 12:50:57 -0700

    Set the result to TextView, pretty-printed

--- src/com/jasonkliu/qualtricsapi/MainActivity.java
+++ src/com/jasonkliu/qualtricsapi/MainActivity.java
@@ -7,8 +7,14 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
@@ -20,10 +26,14 @@ public class MainActivity extends Activity {
         return (netInfo != null && netInfo.isConnectedOrConnecting());
     }
 
+    TextView display;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        display = (TextView) findViewById(R.id.tvDisplay);
 
         Uri.Builder builder = new Uri.Builder();
         builder.scheme("https")
@@ -56,10 +66,27 @@ public class MainActivity extends Activity {
                     @Override
                     public void onCompleted(Exception e, JsonObject result) {
                         // do stuff with the result or error
-                        if (result != null)
+                        if (result != null) {
                             Log.d("JSONresult", result.toString());
-                        else
+
+                            /* Ion uses the Gson Library, which makes pretty printing hard (it's
+                             * normally json.toString(4) in org.json libraries.
+                             *
+                             * Below is a one-liner of the following:
+                             * Gson gs = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+                             * JsonParser parser = new JsonParser();
+                             * JsonElement je = parser.parse(result.toString());
+                             * display.setText(gs.toJson(je));
+                             *
+                             * https://stackoverflow.com/questions/4105795
+                             * https://stackoverflow.com/questions/11020894
+                             */
+
+                            display.setText(new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create()
+                                    .toJson(new JsonParser().parse(result.toString())));
+                        } else {
                             Log.d("JSONexcept", e.toString());
+                        }
                     }
                 });
         }
