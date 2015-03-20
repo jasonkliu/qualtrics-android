599fb17fecf338feddfdf58fe59f042c8e946cbf
Jason Liu <jason@jasonkliu.com>
2015-03-19 21:07:47 -0700

    Refactor: move comments and isOnline method

--- src/com/jasonkliu/qualtricsapi/MainActivity.java
+++ src/com/jasonkliu/qualtricsapi/MainActivity.java
@@ -61,96 +61,96 @@ public class MainActivity extends Activity {
         // .appendQueryParameter("User", "user#group")
         // .appendQueryParameter("Token", "token")
 
+        /* Result is of type:
+         * {
+         *    "Meta": {
+         *        "Status": "Success",
+         *        "Debug": ""
+         *    },
+         *    "Result": {
+         *        "Surveys": [
+         *            {
+         *                "responses": "1",
+         *                "SurveyType": "SV",
+         *                "SurveyID": "SV_eidnumber",
+         *                "SurveyName": "SurveyName",
+         *                "SurveyDescription": "SurveyDescritption",
+         *                "SurveyOwnerID": "UR_aidnumber",
+         *                "SurveyStatus": "Active",
+         *                "SurveyStartDate": "0000-00-00 00:00:00",
+         *                "SurveyExpirationDate": "0000-00-00 00:00:00",
+         *                "SurveyCreationDate": "2015-03-19 12:06:13",
+         *                "CreatorID": "UR_aidnumber",
+         *                "LastModified": "2015-03-19 12:07:06",
+         *                "LastActivated": "2015-03-19 12:07:06",
+         *                "UserFirstName": "User",
+         *                "UserLastName": "Name"
+         *            }
+         *            {
+         *                ...
+         *            }
+         *        ]
+         *    }
+         *}
+         */
+
         JsonObject json = new JsonObject();
         json.addProperty("User", new DeveloperKey().USER_ID);
         json.addProperty("Token", new DeveloperKey().DEVELOPER_KEY);
 
-        if (isOnline()) {
-            Ion.with(this)
-                .load(myUrl)
-                .setJsonObjectBody(json)
-                .asJsonObject()
-                .setCallback(new FutureCallback<JsonObject>() {
-                    @Override
-                    public void onCompleted(Exception e, JsonObject result) {
-                        // do stuff with the result or error
-                        if (result != null) {
-                            Log.d("JSONresult", result.toString());
+        if (!isOnline()) { Log.d("onCreate", "No internet"); return; }
 
-                            /* Ion uses the Gson Library, which makes pretty printing hard (it's
-                             * normally json.toString(4) in org.json libraries.
-                             *
-                             * Below is a one-liner of the following:
-                             * Gson gs = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-                             * JsonParser parser = new JsonParser();
-                             * JsonElement je = parser.parse(result.toString());
-                             * display.setText(gs.toJson(je));
-                             *
-                             * https://stackoverflow.com/questions/4105795
-                             * https://stackoverflow.com/questions/11020894
-                             */
+        Ion.with(this)
+            .load(myUrl)
+            .setJsonObjectBody(json)
+            .asJsonObject()
+            .setCallback(new FutureCallback<JsonObject>() {
+                @Override
+                public void onCompleted(Exception e, JsonObject result) {
+                    if (result != null) {
+                        Log.d("JSONresult", result.toString());
 
-                            display.setText(new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create()
-                                    .toJson(new JsonParser().parse(result.toString())));
+                        /* Ion uses the Gson Library, which makes pretty printing hard (it's
+                         * normally json.toString(4) in org.json libraries.
+                         *
+                         * Below is a one-liner of the following:
+                         * Gson gs = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+                         * JsonParser parser = new JsonParser();
+                         * JsonElement je = parser.parse(result.toString());
+                         * display.setText(gs.toJson(je));
+                         *
+                         * https://stackoverflow.com/questions/4105795
+                         * https://stackoverflow.com/questions/11020894
+                         */
 
-                            /* Result is of type:
-                             * {
-                             *    "Meta": {
-                             *        "Status": "Success",
-                             *        "Debug": ""
-                             *    },
-                             *    "Result": {
-                             *        "Surveys": [
-                             *            {
-                             *                "responses": "1",
-                             *                "SurveyType": "SV",
-                             *                "SurveyID": "SV_eidnumber",
-                             *                "SurveyName": "SurveyName",
-                             *                "SurveyDescription": "SurveyDescritption",
-                             *                "SurveyOwnerID": "UR_aidnumber",
-                             *                "SurveyStatus": "Active",
-                             *                "SurveyStartDate": "0000-00-00 00:00:00",
-                             *                "SurveyExpirationDate": "0000-00-00 00:00:00",
-                             *                "SurveyCreationDate": "2015-03-19 12:06:13",
-                             *                "CreatorID": "UR_aidnumber",
-                             *                "LastModified": "2015-03-19 12:07:06",
-                             *                "LastActivated": "2015-03-19 12:07:06",
-                             *                "UserFirstName": "User",
-                             *                "UserLastName": "Name"
-                             *            }
-                             *            {
-                             *                ...
-                             *            }
-                             *        ]
-                             *    }
-                             *}
-                             */
+                        display.setText(new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create()
+                                .toJson(new JsonParser().parse(result.toString())));
 
-                            JsonArray ja = result.getAsJsonObject("Result").getAsJsonArray("Surveys");
-                            Log.d("JSONObject", ja.toString());
+                        // Extract the Surveys JsonArray, and specifically each SurveyID
+                        JsonArray ja = result.getAsJsonObject("Result").getAsJsonArray("Surveys");
+                        // Log.d("JsonArray", ja.toString());
 
-
-
-                            for (final JsonElement survey : ja) {
-                                final JsonObject each = survey.getAsJsonObject();
-                                lastsurveyid = each.getAsJsonObject().getAsJsonPrimitive("SurveyID").toString().replaceAll("\"", "");  // remove double quotes
-                                Log.d("new", lastsurveyid);
-                            }
-
-                            linksurvey.setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-                                    Uri uriUrl1 = Uri.parse("https://yalesurvey.qualtrics.com/SE/?SID=" + lastsurveyid);
-                                    Intent launchBrowser1 = new Intent(Intent.ACTION_VIEW, uriUrl1);
-                                    startActivity(launchBrowser1);
-                                }
-                            });
-                        } else {
-                            Log.d("JSONexcept", e.toString());
+                        for (final JsonElement survey : ja) {
+                            final JsonObject each = survey.getAsJsonObject();
+                            lastsurveyid = each.getAsJsonObject().getAsJsonPrimitive("SurveyID")
+                                    .toString().replaceAll("\"", "");  // remove double quotes
+                            Log.d("new", lastsurveyid);
                         }
+
+                        // Set clickable button to last survey loaded
+                        linksurvey.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Uri uriUrl1 = Uri.parse("https://yalesurvey.qualtrics.com/SE/?SID=" + lastsurveyid);
+                                Intent launchBrowser1 = new Intent(Intent.ACTION_VIEW, uriUrl1);
+                                startActivity(launchBrowser1);
+                            }
+                        });
+                    } else {
+                        Log.d("JSONexcept", e.toString());
                     }
-                });
-        }
+                }
+            });
     }
 
 }
